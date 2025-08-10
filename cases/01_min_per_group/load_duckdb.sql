25000631d77856c6ad8344cb571dd909b579b21e
Benz <kingtut8008@gmail.com>
2025-08-10 17:09:06 +0300

    Load via INSERT into stock_prices (avoid table-exists)

--- cases/01_min_per_group/load_duckdb.sql
+++ cases/01_min_per_group/load_duckdb.sql
@@ -1,3 +1,6 @@
--- Load the CSV into DuckDB as a table
-CREATE TABLE stock_prices AS
+-- Load rows into the table defined in schema.sql
+-- (optional) clear previous rows if you re-run inside the same session
+DELETE FROM stock_prices;
+
+INSERT INTO stock_prices
 SELECT * FROM read_csv_auto('data.csv', HEADER=TRUE);
